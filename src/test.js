6b1197d0bc618a151348d6ab27750aaf3a3f5434
Alexis Munsayac <alexis@lyon.com.ph>
2019-11-05 15:01:55 +0800

    Update test.js

--- src/test.js
+++ src/test.js
@@ -1,7 +1,19 @@
-import ExampleComponent from './'
+import createModel from '.';
 
-describe('ExampleComponent', () => {
-  it('is truthy', () => {
-    expect(ExampleComponent).toBeTruthy()
-  })
-})
+describe('createModel', () => {
+  it('returns an Object', () => {
+    const model = createModel(() => ({
+      state: {
+
+      },
+      action: {
+
+      },
+    }));
+    expect(model).toBeTruthy();
+    expect(typeof model).toBe('object');
+    expect(typeof model.Provider).toBe('function');
+    expect(typeof model.useState).toBe('function');
+    expect(typeof model.useAction).toBe('function');
+  });
+});
