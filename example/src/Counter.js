0362f7762e62985c0177c5d88c4823b7e5d534e6
Alexis Munsayac <alexis@lyon.com.ph>
2019-11-06 12:44:32 +0800

    Create Counter.js

---
+++ example/src/Counter.js
@@ -0,0 +1,23 @@
+import createModel from '@lxsmnsyc/react-scoped-model';
+import { useState, useCallback } from 'react';
+
+
+const Counter = createModel(function () {
+  const [count, setCount] = useState(0);
+
+  const increment = useCallback(() => {
+    setCount(c => c + 1);
+  }, []);
+
+  const decrement = useCallback(() => {
+    setCount(c => c - 1);
+  }, []);
+
+  return {
+    count,
+    increment,
+    decrement,
+  };
+});
+
+export default Counter;
