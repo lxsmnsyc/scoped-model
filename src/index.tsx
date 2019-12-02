19b5f13e34859150e7d5f4d6c7a4f7a3ed18a681
Alexis Munsayac <alexis@lyon.com.ph>
2019-12-02 18:05:24 +0800

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -77,9 +77,10 @@ function useForceUpdate() {
 // useLayoutEffect in the browser. We need useLayoutEffect because we want
 // `connect` to perform sync updates to a ref to save the latest props after
 // a render is actually committed to the DOM.
-export function useIsomorphicEffect(callback: React.EffectCallback, deps: React.DependencyList) {
-  (typeof window === 'undefined' ? React.useEffect : React.useLayoutEffect)(callback, deps);
-}
+export const useIsomorphicEffect = typeof window === 'undefined'
+  ? React.useEffect
+  : React.useLayoutEffect
+;
 
 export default function createModel<M extends IModelState, P extends {}>(modelHook: ModelHook<M, P>) {
   /**
