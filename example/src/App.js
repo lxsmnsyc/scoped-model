cb0b6473ed021e648c59f29e6bcb0f9d07cf9d27
Alexis Munsayac <alexis@lyon.com.ph>
2020-01-26 15:35:02 +0800

    Update App.js

--- example/src/App.js
+++ example/src/App.js
@@ -26,6 +26,53 @@ function Decrement() {
   );
 }
 
+const sleep = time => new Promise((res) => setTimeout(res, time));
+
+function SuspendedCount() {
+  const count = Counter.useSuspendedSelector(async (state) => {
+    await sleep(500);
+
+    return state.count;
+  }, 'count/suspended');
+
+  console.log('Rendered SuspendedCount');
+
+  return (
+    <h1>Count: {count}</h1>
+  );
+}
+
+function SuspendedState() {
+  const count = Counter.useSuspendedState((state) => ({
+    value: state.count,
+    suspend: state.count < 5,
+  }), 'state/suspended');
+
+  console.log('Rendered SuspendedState');
+
+  return (
+    <h1>Count: {count}</h1>
+  );
+}
+
+function AsyncCount() {
+  const state = Counter.useAsyncSelector(async (state) => {
+    await sleep(500);
+
+    return state.count;
+  });
+
+  console.log('Rendered AsyncCount');
+
+  switch (state.status) {
+    case 'failure': return <h1>An error occured.</h1>;
+    case 'success': return <h1>Count: {state.data}</h1>;
+    case 'pending': return <h1>Loading...</h1>;
+    default: 
+  }
+}
+
+
 function IncDec() {
   const [increment, decrement] = Counter.useProperties(['increment', 'decrement']);
   console.log('Re-rendered IncDec');
@@ -42,6 +89,28 @@ export default function App() {
     <Counter.Provider initialCount={1}>
       <Counter.Provider>
         <Count />
+        <Increment />
+        <Decrement />
+      </Counter.Provider>
+      <Counter.Provider>
+        <h1>Suspended Data:</h1>
+        <React.Suspense fallback={<h1>Loading...</h1>}>
+          <SuspendedCount />
+        </React.Suspense>
+        <Increment />
+        <Decrement />
+      </Counter.Provider>
+      <Counter.Provider>
+        <h1>Suspended State:</h1>
+        <React.Suspense fallback={<h1>Loading...</h1>}>
+          <SuspendedState />
+        </React.Suspense>
+        <Increment />
+        <Decrement />
+      </Counter.Provider>
+      <Counter.Provider>
+        <h1>Async Data:</h1>
+        <AsyncCount />
         <Increment />
         <Decrement />
       </Counter.Provider>
