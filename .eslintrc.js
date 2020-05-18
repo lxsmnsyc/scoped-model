1c41a667aed66dedf8b8da2164a00eda0ccb0b3e
Alexis Munsayac <alexis.munsayac@gmail.com>
2020-05-18 14:27:58 +0800

    Update project setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,63 +1,60 @@
 module.exports = {
-    env: {
-      browser: true,
-      es6: true,
-      node: true,
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'airbnb/hooks',
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    ecmaFeatures: {
+      jsx: true,
     },
-    extends: [
-      'plugin:react/recommended',
-      'airbnb',
-      'airbnb/hooks',
-      "plugin:@typescript-eslint/eslint-recommended",
-      "plugin:@typescript-eslint/recommended",
-      "plugin:@typescript-eslint/recommended-requiring-type-checking",
-      "plugin:import/errors",
-      "plugin:import/warnings",
-      "plugin:import/typescript",
-    ],
-    globals: {
-      Atomics: 'readonly',
-      SharedArrayBuffer: 'readonly',
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+  ],
+  settings: {
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"]
     },
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-      project: "./tsconfig.json",
-      ecmaFeatures: {
-        jsx: true,
-      },
-      ecmaVersion: 2018,
-      sourceType: 'module',
-    },
-    plugins: [
-      'react',
-      '@typescript-eslint',
-    ],
-    settings: {
-      "import/parsers": {
-        "@typescript-eslint/parser": [".ts", ".tsx"]
-      },
-      "import/resolver": {
-        "typescript": {
-          "alwaysTryTypes": true // always try to resolve types under `<roo/>@types` directory even it doesn't contain any source code, like `@types/unist`
-        },
+    "import/resolver": {
+      "typescript": {}
+    }
+  },
+  rules: {
+    "@typescript-eslint/no-explicit-any": "off",
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      {
+        "js": "never",
+        "jsx": "never",
+        "ts": "never",
+        "tsx": "never"
       }
-    },
-    rules: {
-      "@typescript-eslint/interface-name-prefix": "off",
-      "@typescript-eslint/no-explicit-any": "off",
-      "@typescript-eslint/explicit-function-return-type": "off",
-      "import/extensions": [
-        "error",
-        "ignorePackages",
-        {
-          "js": "never",
-          "jsx": "never",
-          "ts": "never",
-          "tsx": "never"
-        }
-      ],
-      "react/jsx-filename-extension": [1, { "extensions": [".jsx", ".tsx"] }],
-      "react/jsx-props-no-spreading": "off",
-    },
-  };
-  
+    ],
+    "import/no-unresolved": "error",
+    "@typescript-eslint/interface-name-prefix": "off",
+    "react/jsx-props-no-spreading": "off",
+    "react/jsx-filename-extension": [1, { "extensions": [".jsx", ".tsx"] }],
+  },
+};
