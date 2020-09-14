d5971b9c58f7a2fdf6403e102fd240b5d0d8e1ae
Alexis Munsayac <alexis.munsayac@gmail.com>
2020-09-14 22:20:46 +0800

    Update eslintrc for react-scoped-model

---
+++ packages/react-scoped-model/.eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  "root": true,
+  "extends": [
+    "lxsmnsyc/typescript/react"
+  ],
+  "parserOptions": {
+    "project": "./tsconfig.eslint.json",
+    "tsconfigRootDir": __dirname,
+  },
+  "rules": {
+    "import/no-extraneous-dependencies": [
+      "error", {
+        "devDependencies": ["**/*.test.tsx"]
+      }
+    ],
+    "react/jsx-props-no-spreading": "off",
+  },
+};
+
