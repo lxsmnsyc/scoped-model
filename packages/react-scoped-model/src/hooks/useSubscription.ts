ec4c30a1fefda6fb99981598e4039745dcb798a6
Alexis Munsayac <alexis.munsayac@gmail.com>
2020-11-06 15:30:42 +0800

    Fix value reading for stale subscriptions

--- packages/react-scoped-model/src/hooks/useSubscription.ts
+++ packages/react-scoped-model/src/hooks/useSubscription.ts
@@ -1,3 +1,30 @@
+/**
+ * @license
+ * MIT License
+ *
+ * Copyright (c) 2020 Alexis Munsayac
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ *
+ * @author Alexis Munsayac <alexis.munsayac@gmail.com>
+ * @copyright Alexis Munsayac 2020
+ */
 import { useDebugValue } from 'react';
 import { Compare, defaultCompare } from '../utils/comparer';
 import useFreshState from './useFreshState';
@@ -32,8 +59,6 @@ export default function useSubscription<T>({
 
     const readCurrent = () => {
       if (mounted) {
-        const nextValue = read();
-
         setState((prev) => {
           if (
             prev.read !== read
@@ -42,6 +67,7 @@ export default function useSubscription<T>({
           ) {
             return prev;
           }
+          const nextValue = read();
           if (!shouldUpdate(prev.value, nextValue)) {
             return prev;
           }
