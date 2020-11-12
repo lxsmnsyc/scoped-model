99ff7e64709b0638321dbc07b6bd8555be2e9085
Alexis Munsayac <alexis.munsayac@gmail.com>
2020-11-12 19:47:37 +0800

    Add ZoomOut

--- packages/graph-state-devtools/src/NetworkVisualizerControls.tsx
+++ packages/graph-state-devtools/src/NetworkVisualizerControls.tsx
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 
 import {
   useGraphNodeReset,
+  useGraphNodeValue,
 } from 'react-graph-state';
 
 import { Button, ButtonGroup } from '@geist-ui/react';
-import { RefreshCw } from '@geist-ui/react-icons';
+import { ZoomOut, RefreshCw } from '@geist-ui/react-icons';
 
 import refresh from './nodes/refresh';
+import network from './nodes/network';
+
+function ZoomOutControl(): JSX.Element {
+  const currentNetwork = useGraphNodeValue(network);
+
+  const onClick = useCallback(() => {
+    if (currentNetwork) {
+      currentNetwork.fit({
+        animation: true,
+      });
+    }
+  }, [currentNetwork]);
+
+  return (
+    <Button
+      iconRight={<ZoomOut />}
+      onClick={onClick}
+    />
+  );
+}
 
 function RefreshControl(): JSX.Element {
   const resetRefresh = useGraphNodeReset(refresh);
@@ -23,6 +44,7 @@ function RefreshControl(): JSX.Element {
 export default function NetworkVisualizerControls(): JSX.Element {
   return (
     <ButtonGroup>
+      <ZoomOutControl />
       <RefreshControl />
     </ButtonGroup>
   );
