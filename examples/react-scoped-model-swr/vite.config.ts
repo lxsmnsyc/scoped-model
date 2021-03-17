4645e97c0b7e9ef4c12cd3b9bd7786a543a82e50
Alexis Munsayac <alexis.munsayac@gmail.com>
2021-03-17 12:31:29 +0800

    Update react-scoped-model-swr

--- examples/react-scoped-model-swr/vite.config.ts
+++ examples/react-scoped-model-swr/vite.config.ts
@@ -1,15 +1,13 @@
-import * as reactPlugin from 'vite-plugin-react';
-import type { UserConfig } from 'vite';
+import { defineConfig } from 'vite'
+import reactRefresh from '@vitejs/plugin-react-refresh'
 
-const config: UserConfig = {
-  jsx: 'react',
-  plugins: [reactPlugin],
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [reactRefresh()],
   optimizeDeps: {
     exclude: [
       'react-scoped-model',
       'react-scoped-model-swr',
     ],
-  },
-};
-
-export default config;
+  }
+});
