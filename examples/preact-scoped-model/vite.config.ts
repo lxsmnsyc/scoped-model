fac547221cba07bb80e155b358dd53f976e5cb7c
Alexis Munsayac <alexis.munsayac@gmail.com>
2021-04-30 23:49:14 +0800

    Update dependencies

--- examples/preact-scoped-model/vite.config.ts
+++ examples/preact-scoped-model/vite.config.ts
@@ -1,17 +1,6 @@
-import { defineConfig } from 'vite'
-import preactRefresh from '@prefresh/vite'
+import { defineConfig } from "vite";
+import preact from "@preact/preset-vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  esbuild: {
-    jsxFactory: 'h',
-    jsxFragment: 'Fragment',
-    jsxInject: `import { h, Fragment } from 'preact'`
-  },
-  plugins: [preactRefresh()],
-  optimizeDeps: {
-    exclude: [
-      'preact-scoped-model',
-    ],
-  },
+  plugins: [preact()]
 });
