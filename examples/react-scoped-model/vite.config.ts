baf51d1dd9dc910def849b99592f91d4a7d16e2f
Alexis Munsayac <alexis.munsayac@gmail.com>
2021-05-06 21:09:33 +0800

    Update vite config

--- examples/react-scoped-model/vite.config.ts
+++ examples/react-scoped-model/vite.config.ts
@@ -4,9 +4,4 @@ import reactRefresh from '@vitejs/plugin-react-refresh'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh()],
-  optimizeDeps: {
-    exclude: [
-      'react-scoped-model',
-    ],
-  }
 });
